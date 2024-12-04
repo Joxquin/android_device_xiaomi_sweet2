1efd843abb22c10fb8b1e97bd5a74033bd00f76f
Mathieu Chartier <mathieuc@google.com>
2024-12-03 19:07:22 -0500

    sweet2: Always preopt extracted APKs

--- device.mk
+++ device.mk
@@ -90,6 +90,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
 # System properties
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/properties/recovery.prop:$(TARGET_COPY_OUT_RECOVERY)/root/recovery.prop
